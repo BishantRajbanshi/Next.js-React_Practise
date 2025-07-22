212aea316f16e43cee1680b91610aa1a3c81de98
Bishant Rajbanshi <rajbancbishant@gmail.com>
2025-07-22 08:58:44 +0545

    versel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
+import { Analytics } from "@vercel/analytics/next"
 import Dropdown from "./components/dropdown";
 
 const geistSans = Geist({
